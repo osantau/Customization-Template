31bb86ccd22c0b0ee4ff52242061934d206f4372
Octavian Santau <octavian.santau.os@gmail.com>
2020-03-02 19:59:10 +0200

    generate only customization.jar

---
+++ base/src/org/compiere/helper/GeneralHelper.java
@@ -0,0 +1,21 @@
+package org.compiere.helper;
+
+import java.util.UUID;
+
+import org.compiere.model.MInvoice;
+import org.compiere.process.SvrProcess;
+
+public class GeneralHelper extends SvrProcess {
+
+	@Override
+	protected void prepare() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String doIt() throws Exception {
+		return null;
+	}
+
+}
